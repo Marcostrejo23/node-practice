0f0ad92e5523bc689deb411652334d95f6b14250
Marcos Trejo <marcostrejo23@gmail.com>
2022-04-14 14:45:20 -0700

    map method example

--- twoSum.js
+++ twoSum.js
@@ -8,4 +8,15 @@ const twoSum = function(nums, target){
         }
         map.set(num1, i);
     }
-}
+};
+
+
+//map example 
+
+const array1 = [1, 4, 9, 16];
+
+// pass a function to map
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
+// expected output: Array [2, 8, 18, 32]
